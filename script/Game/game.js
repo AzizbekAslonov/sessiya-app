87ec79d7c15748ceb18b930be41fcb484534fa1d
AzizbekAslonov <aslonovazizbek318@gmail.com>
2023-06-19 21:48:34 +0500

    asd

--- script/Game/game.js
+++ script/Game/game.js
@@ -1,13 +1,20 @@
+const localName = localStorage.getItem('name')
+
 // Tanlangan fan
 let localScience = localStorage.getItem('science');
 
 if (realSciences.includes(localScience)) {
-   const a = new Questions(localScience, {
-      eachAddingScore: 1,
-      count: 25,
-      variants: ['A', 'B', 'C', 'D'],
-      // By default -> random: true,
-      randomAnswers: true,
-   })
-   a.startGame()
+   if (localName) {
+      const a = new Questions(localScience, {
+         eachAddingScore: 1,
+         count: 25,
+         variants: ['A', 'B', 'C', 'D'],
+         // By default -> random: true,
+         randomAnswers: true,
+      })
+      a.startGame()
+   }
+   else {
+      location.assign('index.html')
+   }
 }
