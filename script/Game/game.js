dde85413292d442398923aa73a6aeaca9a4cc0a2
Asadbek <adavlatov@ticketboat.com>
2026-05-15 16:32:05 +0500

    add a bit of safety

--- script/Game/game.js
+++ script/Game/game.js
@@ -4,28 +4,28 @@ let localScience = localStorage.getItem('science');
 if (BANNED_USERS.includes(localName)) {
    alert(BANNED_MESSAGE + " " + localName.split(' ')[0] + "?")
    location.assign('index.html')
-}
-
-if (!checkName(localName)) {
-   alert(CHECK_NAME_MESSAGE)
-   location.assign('index.html')
-}
-
-if (localScience in ALL_QUESTIONS) {
-   if (localName) {
-      let gameApp = new Questions(localScience, {
-         eachAddingScore: 1,
-         count: QUESTIONS_EACH_ROUND,
-         testProgram: ALL_QUESTIONS[localScience],
-         variants: ['A', 'B', 'C', 'D'],
-         // By default -> random: true,
-         randomAnswers: true,
-      })
-      gameApp.startGame()
-   }
-   else {
+} else {
+   if (!checkName(localName)) {
+      alert(CHECK_NAME_MESSAGE)
       location.assign('index.html')
    }
-} else {
-   alert(`Bunday fan mavjud emas`)
+   
+   if (localScience in ALL_QUESTIONS) {
+      if (localName) {
+         let gameApp = new Questions(localScience, {
+            eachAddingScore: 1,
+            count: QUESTIONS_EACH_ROUND,
+            testProgram: ALL_QUESTIONS[localScience],
+            variants: ['A', 'B', 'C', 'D'],
+            // By default -> random: true,
+            randomAnswers: true,
+         })
+         gameApp.startGame()
+      }
+      else {
+         location.assign('index.html')
+      }
+   } else {
+      alert(`Bunday fan mavjud emas`)
+   }
 }
--- script/index.js
+++ script/index.js
@@ -9,49 +9,49 @@ const nameInput = document.getElementById("nameInput")
 const localName = localStorage.getItem('name');
 
 if (BANNED_USERS.includes(localName)) {
-   alert(BANNED_MESSAGE + " " + localName.split(' ')[0] + "?")
-   location.assign('index.html')
+   // alert(BANNED_MESSAGE + " " + localName.split(' ')[0] + "?")
+   // location.assign('index.html')
+} else {
+   if (checkName(localName)) {
+      nameInput.value = localName;
+    }
+    
+    const shouldSend = document.getElementById("shouldSend")
+    const localCompetition = localStorage.getItem('competition')
+    if (shouldSend) {
+       if (localCompetition === "true") {
+          shouldSend.checked = true
+       } else if (localCompetition === "false") {
+          shouldSend.checked = false
+       } else {
+          shouldSend.checked = true
+       }  
+    }
+    
+    btnCollection.forEach(btn => {
+       const buttonEl = DOMElement({
+          tag: 'a',
+          value: btn.data + ' ',
+          classes: btn.classes,
+          attributes: ['data-science', btn.data],
+          children: DOMElement({ tag: 'i', classes: btn.icon || '' })
+       })
+    
+       buttonEl.addEventListener('click', () => {
+          if (checkName(nameInput.value)) {
+             const nameInput = document.getElementById("nameInput")
+             const shouldSend = document.getElementById("shouldSend")
+    
+             localStorage.setItem('name', nameInput.value)
+             localStorage.setItem('science', btn.data)
+             localStorage.setItem('competition', shouldSend.checked)
+    
+             location.assign('game.html')
+          } else {
+             alert(CHECK_NAME_MESSAGE)
+          }
+       })
+    
+       buttons.insertAdjacentElement('beforeend', buttonEl)
+    })
 }
-
-if (checkName(localName)) {
-  nameInput.value = localName;
-}
-
-const shouldSend = document.getElementById("shouldSend")
-const localCompetition = localStorage.getItem('competition')
-if (shouldSend) {
-   if (localCompetition === "true") {
-      shouldSend.checked = true
-   } else if (localCompetition === "false") {
-      shouldSend.checked = false
-   } else {
-      shouldSend.checked = true
-   }  
-}
-
-btnCollection.forEach(btn => {
-   const buttonEl = DOMElement({
-      tag: 'a',
-      value: btn.data + ' ',
-      classes: btn.classes,
-      attributes: ['data-science', btn.data],
-      children: DOMElement({ tag: 'i', classes: btn.icon || '' })
-   })
-
-   buttonEl.addEventListener('click', () => {
-      if (checkName(nameInput.value)) {
-         const nameInput = document.getElementById("nameInput")
-         const shouldSend = document.getElementById("shouldSend")
-
-         localStorage.setItem('name', nameInput.value)
-         localStorage.setItem('science', btn.data)
-         localStorage.setItem('competition', shouldSend.checked)
-
-         location.assign('game.html')
-      } else {
-         alert(CHECK_NAME_MESSAGE)
-      }
-   })
-
-   buttons.insertAdjacentElement('beforeend', buttonEl)
-})
